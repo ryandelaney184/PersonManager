1481145b80ff39c4c2e8f35e6903b4c465f415bb
Ryan Delaney <ryandelaney184@gmail.com>
2024-03-31 16:08:01 -0700

    Added the basic functionality, and stlye to the app component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,78 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Employee} from "./objects/employee";
+import {NgForOf} from "@angular/common";
+import {FormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgForOf, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'PersonManager';
+
+  //--------------------------------
+  // Attributes
+  //--------------------------------
+  title = 'Employee Manager';
+
+  @Input()
+  employeeViewList: Array<Employee> = new Array<Employee>();
+
+  employeeList:Array<Employee> = new Array<Employee>();
+
+  //--------------------------------
+  // Constructor
+  //--------------------------------
+
+  /**
+   * The constructor will load some default Employee into the view list.
+   */
+  constructor() {
+    let employee1 = new Employee();
+    employee1.Name = "John Doe";
+    employee1.Position = "Teardown";
+
+    let employee2: Employee = new Employee();
+    employee2.Name = "Jane Doe";
+    employee2.Position = "Teardown";
+
+    let employee3: Employee = new Employee();
+    employee3.Name = "Jerry Gut";
+    employee3.Position = "Public Nuisance";
+
+    this.employeeList.push(employee1, employee2, employee3);
+
+    for (const employeeListElement of this.employeeList) {
+      this.employeeViewList.push(employeeListElement);
+    }
+  }
+
+  //--------------------------------
+  // Methods
+  //--------------------------------
+  /**
+   * The search method will update the list of employee that are viewed on the
+   * web page. If none are found this will return a empty list, and nothing
+   * should show up/
+   * @param event The change event.
+   */
+  search(event: Event) {
+    for (const employeeListElement of this.employeeList) {
+     this.employeeViewList.pop();
+    }
+
+    for (const employee of this.employeeList) {
+
+      let valEle = event.target as HTMLInputElement;
+
+      if(employee.Name.indexOf(valEle.value) != -1 ||
+        employee.Position.indexOf(valEle.value) != -1){
+        this.employeeViewList.push(employee);
+      }
+    }
+  }
+
 }
