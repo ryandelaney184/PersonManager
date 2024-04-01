e66fc12b9b8fd7d38e7375ce49333646282ae92c
Ryan Delaney <32099936+ryandelaney184@users.noreply.github.com>
2024-03-31 18:26:23 -0700

    Update employee.spec.ts

--- src/app/objects/employee.spec.ts
+++ src/app/objects/employee.spec.ts
@@ -3,10 +3,10 @@ import { Employee } from './employee';
 describe('Employee', () => {
   it('should create an instance', () => {
 
-    let Andrew: Employee = new Employee();
-    Andrew.Name = "John Doe";
-    Andrew.Position = "Teardown";
+    let employee: Employee = new Employee();
+    employee.Name = "John Doe";
+    employee.Position = "Teardown";
 
-    expect(Andrew).toBeTruthy();
+    expect(employee).toBeTruthy();
   });
 });
